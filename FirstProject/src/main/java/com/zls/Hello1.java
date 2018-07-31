2e58c5f9eb97f64c6eac748d8f7cd8b767e569f6
liuconghua <lchzls@163.com>
2018-07-31 20:22:35 +0800

    commit a project

---
+++ FirstProject/src/main/java/com/zls/Hello1.java
@@ -0,0 +1,13 @@
+package com.zls;
+
+/**
+ * @author liuconghua
+ * @email liuconghua@iboxpay.com
+ * @date 2018/7/31 10:23
+ */
+public class Hello1 {
+    public static void main(String[] args) {
+        System.out.println("ok---");
+        System.out.println("HelloWorld");
+    }
+}
